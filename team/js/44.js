048e9b2f59164408dba7f557a9a335635be0ce0d
FriendC <794728275@qq.com>
2015-07-06 10:24:58 +0800

    上传了一些TEAM文件

---
+++ team/js/44.js
@@ -0,0 +1,9 @@
+	(function($){
+		$(function(){
+			$('#navigation').lavalamp({
+				gap:15,
+				easeinel:'easeInOutCirc',				//设置options.easing为ease InOutElastic
+				ease:''
+			});
+		});
+	})(jQuery);
